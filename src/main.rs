8c67b40fc1e2600618e80739261991971a94870b
sharkdp <davidpeter@web.de>
2019-06-22 18:27:17 +0200

    Add two command-line arguments for 'pattern' and 'replacement'

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use clap::{App, AppSettings, crate_description, crate_name, crate_version};
+use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
 
 fn main() {
     let app = App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
-        .global_setting(AppSettings::ColoredHelp);
-    app.get_matches();
+        .global_setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("pattern")
+                .required(true)
+                .help("The search pattern that should be replaced"),
+        )
+        .arg(
+            Arg::with_name("replacement")
+                .required(true)
+                .help("The string that should be substituted in"),
+        );
 
-    println!("Hello, world!");
+    let matches = app.get_matches();
+
+    dbg!(matches.value_of("pattern"));
+    dbg!(matches.value_of("replacement"));
 }
