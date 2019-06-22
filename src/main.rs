50714e824d2feb320c66a3384548a12529e3367d
sharkdp <davidpeter@web.de>
2019-06-22 18:17:45 +0200

    Add clap dependency, initial CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{App, AppSettings, crate_description, crate_name, crate_version};
+
 fn main() {
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .global_setting(AppSettings::ColoredHelp);
+    app.get_matches();
+
     println!("Hello, world!");
 }
