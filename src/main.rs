54236008f266b6b2469706218d1eb12be2189775
sharkdp <davidpeter@web.de>
2019-06-22 23:49:52 +0200

    Rudimentary error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 use std::io;
 use std::io::prelude::*;
+use std::process;
 
 use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
+
+#[derive(Debug, Clone, Copy)]
+enum SubError {
+    FailedToWrite,
+    InvalidUTF8,
+}
+
+impl SubError {
+    pub fn message(self) -> String {
+        use SubError::*;
+
+        match self {
+            FailedToWrite => "Output stream has been closed".into(),
+            InvalidUTF8 => "Input contains invalid UTF-8".into(),
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, SubError>;
+
+fn run(pattern: &str, replacement: &str) -> Result<()> {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let input = stdin.lock();
+    let mut output = stdout.lock();
+
+    for line in input.lines() {
+        let line = line.map_err(|_| SubError::InvalidUTF8)?;
+        let new_line = line.replace(pattern, replacement);
+        writeln!(output, "{}", new_line).map_err(|_| SubError::FailedToWrite)?;
+    }
+
+    Ok(())
+}
 
 fn main() {
     let app = App::new(crate_name!())
@@ -24,11 +59,13 @@ fn main() {
     let pattern = matches.value_of("pattern").expect("required argument");
     let replacement = matches.value_of("replacement").expect("required argument");
 
-    let stdin = io::stdin();
+    let result = run(pattern, replacement);
 
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        let new_line = line.replace(pattern, replacement);
-        println!("{}", new_line);
+    match result {
+        Err(e) => {
+            eprintln!("[sub error]: {}", e.message());
+            process::exit(1);
+        }
+        Ok(_) => {}
     }
 }
