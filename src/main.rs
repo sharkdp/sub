17619d466bba13e648e7ee5eb7e30995bb04d715
sharkdp <davidpeter@web.de>
2019-06-22 18:38:27 +0200

    Add simple line-based read from STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::prelude::*;
+
 use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
 
 fn main() {
@@ -18,6 +21,14 @@ fn main() {
 
     let matches = app.get_matches();
 
-    dbg!(matches.value_of("pattern"));
-    dbg!(matches.value_of("replacement"));
+    let pattern = matches.value_of("pattern").expect("required argument");
+    let replacement = matches.value_of("replacement").expect("required argument");
+
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let new_line = line.replace(pattern, replacement);
+        println!("{}", new_line);
+    }
 }
